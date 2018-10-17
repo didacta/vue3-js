b4ea0902922fdce70ee8e34f57f6976c86cc90c7
Igor Yermak <igor.yermak@gmail.com>
2018-10-16 23:55:08 -0400

    added map and proper links

--- dev/src/App.vue
+++ dev/src/App.vue
@@ -2,19 +2,23 @@
 <template>
  <v-app id="app">
     <div id="nav" >
-    <v-tabs color="rgb(133, 22, 22)" slider-color="red" grow>
-         <v-tab :key="i" :href="`#tab-${i}`" ripple> Military </v-tab>       
-          <v-tab ripple> Politics </v-tab>       
-         <v-tab ripple> Economy </v-tab>       
+    <v-tabs color="black" slider-color="red" grow>
+         <v-tab :key="i" :href="`#tab-${i}`" ripple> Home </v-tab>       
+          <v-tab ripple> Maps </v-tab>       
+         <v-tab ripple> Literature </v-tab>       
          </v-tabs>
              </div>
     <router-view/>
-       <v-footer dark absolute="true"></v-footer>
+       <v-footer dark absolute="true">  ©Igor Yermak <br>- Pax Romana </v-footer>
  </v-app>  
 </template>
 
 <style lang="scss">
+
 #app {
+  background-image: url('http://people.ucalgary.ca/~vandersp/Courses/maps/fullmap3.jpg');
+   background-color: rgba(200,100,0,.5); // Tint color
+  background-blend-mode: multiply;
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -26,18 +30,12 @@
 #nav {
   display: flex;
   flex-wrap: wrap;
-  padding: 10px;
+  padding: .2px;
   text-align: left;
-  margin-bottom: 20px;
-  border-radius: 1%;
+  margin-bottom: 15px;
   font-family: "Quattrocento", serif;
-  box-shadow: 10px 5px 18px 0px rgba(0, 0, 0, 0.75);
-  background: linear-gradient(
-    bottom-right,
-    rgb(133, 22, 22),
-    rgb(216, 37, 37),
-    grey
-  );
+  box-shadow: 6px 2px 18px 0px rgba(0, 0, 0, 0.75);
+  background-color: transparent;
   
   .logo {
     @media only screen and (max-width: 1150px) {
@@ -46,7 +44,7 @@
   }
   a {
     width: 180px;
-    font-size: 18px;
+    font-size: 19px;
     font-weight: bold;
     color: goldenrod;
     &.router-link-exact-active {
--- dev/src/components/HelloWorld.vue
+++ dev/src/components/HelloWorld.vue
@@ -6,35 +6,38 @@
       
      <div class=card>
               <img alt="Vue logo" src="../../src/assets/logo.png" class="logo">
-
+<h2>Links</h2><hr style="height:1px;border:none;color:#333;background-color:#333;margin-bottom:20px;">
     <h3>Political Life</h3>
   
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">Early Republic</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">Post Imperial  </a></li>
+      <li><a href="https://en.wikipedia.org/wiki/Roman_Republic" target="_blank" rel="noopener">Early Republic</a></li>
+      <li><a href="https://en.wikipedia.org/wiki/Roman_Empire" target="_blank" rel="noopener">Post Imperial  </a></li>
     </ul>
     <h3>Military</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Organization & Ranks </a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Weapons</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Strategy</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Demographics</a></li>
+      <li><a href="https://romanmilitary.net/strategy/structure/" target="_blank" rel="noopener">Organization & Ranks </a></li>
+      <li><a href="http://www.ancientmilitary.com/roman-weapons.htm" target="_blank" rel="noopener">Weapons</a></li>
+      <li><a href="https://en.wikipedia.org/wiki/Roman_infantry_tactics" target="_blank" rel="noopener">Strategy</a></li>
+      <li><a href="hhttps://en.wikipedia.org/wiki/Demography_of_the_Roman_Empire" target="_blank" rel="noopener">Demographics</a></li>
     </ul>
     <h3>Economy</h3>
     <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">Jobs</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">Trade</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">Banking</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">Resources</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Slavery</a></li>
+      <li><a href="http://mariamilani.com/ancient_rome/ancient_roman_jobs.htm" target="_blank" rel="noopener">Jobs</a></li>
+      <li><a href="https://www.nationalgeographic.org/media/roman-empire-road-and-trade-network/" target="_blank" rel="noopener">Trade</a></li>
+      <li><a href="http://www.rsrc.ugent.be/file/24" target="_blank" rel="noopener">Resources</a></li>
+      <li><a href="https://en.wikipedia.org/wiki/Slavery_in_ancient_Rome" target="_blank" rel="noopener">Slavery</a></li>
     </ul>
  </div>
   <div class=bigcard>  <code>
-    The Roman Empire, at its height (c. 117 CE), was the most extensive political and social structure in western civilization.</code></div> 
+    The Roman Empire, at its height (c. 117 CE), was the most extensive political and social structure in western civilization.</code>
+    <img alt="Roman Empire 117 A.D" src="https://imgur.com/QuuD7w1.png" class="map">
+    </div> 
  <div class=card>       <img alt="Vue logo" src="../../src/assets/logo.png" class="logo"> 
- <v-alert :value="true" type="warning" outline="true" dismissible="true">Constantinople has fallen </v-alert>
-  <v-alert :value="true" type="error" outline="true" dismissible="true">The Turks are attacking!</v-alert>
-    <v-alert :value="true" type="success" color="#CE8E8E" dismissible="true">You have repaired the city walls</v-alert>
+ <h2>News</h2><hr style="height:1px;border:none;color:#333;background-color:#333;margin-bottom:20px;">
+
+ <v-alert :value="true" type="warning" outline="true" dismissible="true">Constantinople has fallen. </v-alert>
+  <v-alert :value="true" type="error" outline="true" dismissible="true">Barbarians are attacking!</v-alert>
+    <v-alert :value="true" type="success" color="#CE8E8E" dismissible="true">You have repaired the city walls.</v-alert>
 
 
  </div>
@@ -56,7 +59,15 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 :root {
-  background-color: rgba(205, 0, 0, 1);
+background-image: url('http://666a658c624a3c03a6b2-25cda059d975d2f318c03e90bcf17c40.r92.cf1.rackcdn.com/unsplash_527bf56961712_1.JPG');
+}
+code{
+  background-color:rgb(0, 0, 0);
+  color:rgb(80, 8, 14);
+}
+v-alert{
+    font-family: "Quattrocento", serif;
+
 }
 .cards {
   display: flex;
@@ -70,8 +81,8 @@ export default {
   border-radius: 2%;
   margin: 15px;
   padding: 15px;
-  box-shadow: 10px 5px 18px 0px rgba(0, 0, 0, 0.75);
-  @media only screen and (max-width: 1150px) {
+  box-shadow: 4px 2px 18px 0px rgba(0, 0, 0, 0.75);
+  @media only screen and (max-width: 1250px) {
     display: none;
   }
 }
@@ -79,17 +90,20 @@ export default {
   flex: 0 1 22%;
   background-color: rgb(133, 22, 22);
   border-radius: 2%;
-  margin: 25px;
+  margin: 15px;
   padding: 10px;
-  box-shadow: 10px 5px 18px 0px rgba(0, 0, 0, 0.75);
-  @media only screen and (max-width: 1150px) {
+  box-shadow: 4px 2px 18px 0px rgba(0, 0, 0, 0.75);
+  @media only screen and (max-width: 1250px) {
     flex: 0 1 44%;
   }
   @media only screen and (max-width: 850px) {
     flex: 0 1 200%;
   }
 }
-
+.map{
+  height:400px;
+  width:573.5px;
+}
 .logo {
   top: 50%;
   left: 50%;
@@ -101,12 +115,12 @@ export default {
     display: none;
   }
 }
-h1 {
+h1,h2,h3 {
   font-family: "Quattrocento", serif;
 }
 h3 {
   color: plum;
-  margin: 40px 0 0;
+  margin: 20px 0 0;
 }
 ul {
   padding: 0;
@@ -118,6 +132,10 @@ li {
 }
 a {
   color: goldenrod;
+  font-size: 18px;
+}
+hr {
+  color:red;
 }
 ol {
   list-style-type: upper-roman;
